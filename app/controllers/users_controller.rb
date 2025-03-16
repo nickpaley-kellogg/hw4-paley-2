94026dd37514e2a74670b9a3257dde2d6467a271
nickpaley-kellogg Paley <nick.paley@kellogg.northwestern.edu>
2025-03-16 21:12:57 +0000

    successful login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
-    @user["password"] = params["password"]
-    @user.save
-    redirect_to "/sessions/new"
+    @user.first_name = params["first_name"]
+    @user.last_name = params["last_name"]
+    @user.email = params["email"]
+    @user.password = params["password"]
+    
+    if @user.save
+      redirect_to "/sessions/new"
+    else
+      flash[:error] = "Error: #{@user.errors.full_messages.join(", ")}"
+      redirect_to "/users/new"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_16_204218) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_16_210203) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_16_204218) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
